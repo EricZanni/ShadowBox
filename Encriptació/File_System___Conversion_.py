5a09dfaaf54a1494322cce3f17c74e873d763eeb
Èric Zanni Pallarès <eric.zanni@estudiant.enti.cat>
2025-12-17 11:59:03 +0100

    Convertir llista_bytes a str

--- Encriptació/File_System___Conversion_.py
+++ Encriptació/File_System___Conversion_.py
@@ -1,26 +1,38 @@
 def demanar_arxiu():
-    try:
-        while True:
-            entrada_usuari = input("Arrossega l'arxiu: ")
+    while True:
+        entrada_usuari = input("Arrossega l'arxiu: ")
 
-            nom_net = entrada_usuari.replace("&", "")
-            nom_net = nom_net.replace("'", "").replace('"', "")
-            nom_net = nom_net.strip()
-            llegir_arxiu(nom_net)
-    except FileNotFoundError:
-        print(f"ERROR: No trobo l'arxiu {entrada_usuari}")
-        return []
+        nom_net = entrada_usuari.replace("&", "")
+        nom_net = nom_net.replace("'", "").replace('"', "")
+        nom_net = nom_net.strip()
+        llegir_arxiu(nom_net)
+   
+        return nom_net
 
 
 def llegir_arxiu(nom_net):
     try:
         with open(nom_net, "rb") as f:
             contingut_bytes = f.read()
-        llista_numeros = list(contingut_bytes)
-        print(llista_numeros)
+            llista_bytes = list(contingut_bytes)
+            text_bytes = str(llista_bytes)
+            net_text_byte = text_bytes.replace(",", "").replace("[", "").replace("]","")
+        return net_text_byte
     except FileNotFoundError:
         print(f"ERROR: No trobo l'arxiu {nom_net}")
-        return []
+     
+def llegir_arxiu(nom_net):
+    try:
+        with open(nom_net, "rb") as f:
+            datos_bytes = f.read()  # Aquí tienes un objeto de tipo 'bytes'
+            
+        # Decodificamos los bytes directamente a texto
+        texto = datos_bytes.decode("utf-8") 
+        return texto
+    except FileNotFoundError:
+        print(f"ERROR: No trobo l'arxiu {nom_net}")
 
    
-demanar_arxiu()
+arxiu = demanar_arxiu()
+text_bytes = llegir_arxiu(arxiu)
+
