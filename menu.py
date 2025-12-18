4b13642ee333ac4829dba73342a4624880894e12
Èric Zanni Pallarès <eric.zanni@estudiant.enti.cat>
2025-12-18 13:05:31 +0100

    menu.py  SCRUM 4

---
+++ menu.py
@@ -0,0 +1,52 @@
+
+def menu():
+    print("           ShadowBox        ")
+    print("----------------------------")
+    print("         1. Xifrar\n        ")
+    print("       2. Desxifrar\n      ")
+    print("    3. Consultar Hashes\n ")
+    print(" 4. Ocultar textos en imatges\n ")
+
+def seleccionar_opcio():
+    print()
+    while True:
+        seleccio_funcio = int(input("Selecciona la funcio que vols utilitzar: "))
+        if seleccio_funcio == 1:
+            xifrar()
+            break
+        elif seleccio_funcio == 2:
+            desxifrar()
+            break
+        elif seleccio_funcio == 3:
+            consultar_hashes()
+            break
+        elif seleccio_funcio == 4:
+            ocultar_text()
+            break
+        else:
+            print("ERROR: Has de posar el numero corresponent de la funcio que vols utilitzar")
+            
+
+        
+
+
+def xifrar():
+    print("         XIFRAR          ")
+    print("-------------------------")
+
+def desxifrar():
+    print("         DESXIFRAR          ")
+    print("----------------------------")
+
+def consultar_hashes():
+    from hash.hash import menu_visual_hash, opcio_usuari
+    menu_visual_hash()
+    opcio_usuari()
+
+def ocultar_text():
+    print("         OCULTAR TEXT          ")
+    print("-------------------------------")
+
+
+menu()
+seleccionar_opcio()
