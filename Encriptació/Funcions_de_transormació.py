cd9c23ba31aa0ad050f993dc04011f8c8c5fb457
Arnau Brinas Rivas <arnau.brinas@estudiant.enti.cat>
2025-12-20 15:38:43 +0100

    Descripció funcions de transformació

--- Encriptació/Funcions_de_transormació.py
+++ Encriptació/Funcions_de_transormació.py
@@ -1,34 +1,40 @@
+# Creem la taula de substitució del 255 al 0
 sbox = list(range(255, -1, -1))
 
-def sub_bytes(llista):
-   
+# Funció per substituir cada número de la llista per un altre segons la sbox
+def substituir_nums(llista):
     resultat = []
     for n in llista:
-        nou_valor = sbox[n]
-        resultat.append(nou_valor)
+        nou_valor = sbox[n] # Busquem el valor que hi ha a la posició n de la sbox
+        resultat.append(nou_valor) # Afegim aquest valor a la nostra llista
     return resultat
 
-def inv_sub_bytes(llista):
-    
+# Funció contrària a la substitució
+def invertir_substitució_nums(llista):
     resultat = []
     for n in llista:
-        original = sbox.index(n)
-        resultat.append(original)
+        posicio_original = sbox.index(n) # Busquem en quina posició es troba el número n dins la sbox
+        resultat.append(posicio_original)
     return resultat
 
-def shift_rows(llista):
+# Funció per moure els elements cap a l'esquerra
+def moure_esquerra(llista):
+    if len(llista) < 2: # Si la llista té 0 o 1 elements, no cal moure res
+        return llista
+    return llista[1:] + [llista[0]] # Agafem des del segon element fins al final i hi enganxem el primer element al final
+
+# Funció per moure els elements cap a la dreta
+def moure_dreta(llista):
     if len(llista) < 2:
         return llista
-    return llista[1:] + [llista[0]]
+    return [llista[-1]] + llista[:-1] # L'últim passa a ser el primer
 
-def inv_shift_rows(llista):
-    if len(llista) < 2:
-        return llista
-    return [llista[-1]] + llista[:-1]
-
-def add_round_key(llista, clau):
+# Barrejem la llista amb una clau secreta fent servir l'operació XOR
+def afegeix_clau(llista, clau):
     resultat = []
     for i in range(len(llista)):
-        valor = llista[i] ^ clau[i % len(clau)]
-        resultat.append(valor)
+        # Apliquem l'operador XOR entre la dada i la clau
+        # Utilitzem '%' per si la clau s'ha de repetir (si és més curta que la llista)
+        valor_combinat = llista[i] ^ clau[i % len(clau)]
+        resultat.append(valor_combinat)
     return resultat
