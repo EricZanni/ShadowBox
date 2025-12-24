cd01bcd25d7671ce5fa3d08a86bb2932dee7353d
Arnau Brinas Rivas <arnau.brinas@estudiant.enti.cat>
2025-12-24 18:28:56 +0100

    Modificació de funcions de transformació deixant només les de encriptar

--- encriptar/funcions_de_transformacio.py
+++ encriptar/funcions_de_transformacio.py
@@ -9,25 +9,11 @@ def substituir_nums(llista):
         resultat.append(nou_valor) # Afegim aquest valor a la nostra llista
     return resultat
 
-# Funció contrària a la substitució
-def invertir_substitució_nums(llista):
-    resultat = []
-    for n in llista:
-        posicio_original = sbox.index(n) # Busquem en quina posició es troba el número n dins la sbox
-        resultat.append(posicio_original)
-    return resultat
-
 # Funció per moure els elements cap a l'esquerra
 def moure_esquerra(llista):
     if len(llista) < 2: # Si la llista té 0 o 1 elements, no cal moure res
         return llista
     return llista[1:] + [llista[0]] # Agafem des del segon element fins al final i hi enganxem el primer element al final
-
-# Funció per moure els elements cap a la dreta
-def moure_dreta(llista):
-    if len(llista) < 2:
-        return llista
-    return [llista[-1]] + llista[:-1] # L'últim passa a ser el primer
 
 # Barrejem la llista amb una clau secreta fent servir l'operació XOR
 def afegeix_clau(llista, clau):
