76b2df62a8d901a483f373018389b1671ee03839
Arnau Brinas Rivas <arnau.brinas@estudiant.enti.cat>
2025-12-29 12:12:25 +0100

    Implementació de la recuperació de missatges amagats

---
+++ esteganografia/esteganografia_recuperar.py
@@ -0,0 +1,26 @@
+def recuperar_text(imatge):
+    # Obrim la imatge en mode binari ('rb') per llegir tota la informació del fitxer
+    with open(imatge, "rb") as f:
+        contingut = f.read()
+    
+    separador = b"#####" 
+
+    # Busquem el separador dins del contingut, si hi és, tallem el contingut i la part [0] és la imatge i la part [1] és el text secret.
+    if separador in contingut:
+        missatge_en_bytes = contingut.split(separador)[1]
+        
+        # Convertim els bytes a text (utf-8)
+        text_recuperat = missatge_en_bytes.decode("utf-8")
+        print(f"\nMissatge recuperat amb èxit:")
+        print(f"--- {text_recuperat} ---")
+    else:
+        print("\nERROR: No s'ha trobat cap missatge secret en aquesta imatge.")
+
+# Demanem el fitxer a l'usuari i fem la funció.
+while True:
+    imatge = input("Arrossega la imatge per llegir el missatge: ")
+    if imatge == "":
+        print("ERROR: No s'ha detectat cap imatge")
+    else:
+        recuperar_text(imatge)
+        break
